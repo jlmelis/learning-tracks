ed1bc54618cc2e1687005916cd26fbce12909b6a
jasonmelis <jasonmelis@penguin.lxd>
2020-05-26 15:28:51 -0400

    Using a Fauna UDF to retrieve the track list

--- functions/all-tracks.js
+++ functions/all-tracks.js
@@ -3,45 +3,26 @@ import { gql } from 'apollo-boost';
 
 export async function handler(event, context) {
   const { user } = context.clientContext;
-  const userEmail = user ? user.email : '';
-
-  const TRACK_FIELDS_FRAGMENT = gql`
-    fragment trackFields on Track {
-      id: _id
-      name
-      description
-      userEmail
-      public
-    }
-  `;
+  const userEmail = user ? user.email : null;
 
   const TRACKS_QUERY = gql`  
-    query ($userEmail: String!) {
-      tracksByUserEmail(userEmail: $userEmail) {
+    query($userEmail: String) {
+      trackList (userEmail: $userEmail) {
         data {
-          ...trackFields
-        }
-      }
-      tracksByPublicFlag(public: true) {
-        data {
-          ...trackFields
+          id: _id
+          name
+          description
+          userEmail
+          public
         }
       }
     }
-    ${TRACK_FIELDS_FRAGMENT}
   `;
 
-  const tracks = await query(TRACKS_QUERY, { userEmail });
-
-  // combine tracks into one array
-  // TODO: need to distinguish between public and owned.
-  // may need to pas both arrays separately
-  let tracksByUserEmail = tracks.tracksByUserEmail.data;
-  let tracksByPublicFlag = tracks.tracksByPublicFlag.data;
-  let results = tracksByUserEmail.concat(tracksByPublicFlag);
+  const { trackList } = await query(TRACKS_QUERY, { userEmail });
 
   return {
     statusCode: 200,
-    body: JSON.stringify(results),
+    body: JSON.stringify(trackList.data),
   };
 }
